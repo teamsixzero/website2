974d69216af5cdb508525043b65a1ee3b816f4ae
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-03-10 12:45:38 +1300

    type styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import Helmet from "react-helmet"
-import Layout from "../components/layout"
+import * as React from "react";
+import { Link } from "gatsby";
+import Helmet from "react-helmet";
+import Layout from "../components/layout";
 
 const NotFoundPage = () => {
   return (
@@ -14,12 +14,16 @@ const NotFoundPage = () => {
       <section className="page-header page-header--centered">
         <div className="page-header_copy">
           <h1>Page not found</h1>
-          <p className="h4">Sorry, but the page you were trying to view does not exist.</p>
-          <Link to="/" className="btn">Return home →</Link>
+          <p className="h4">
+            Sorry, but the page you were trying to view does not exist.
+          </p>
+          <Link to="/" className="btn">
+            Return home →
+          </Link>
         </div>
       </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
