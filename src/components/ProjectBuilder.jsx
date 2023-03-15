1f88ff462a156396c35dde3d28ffc9414e6a388e
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-03-15 14:28:41 +1300

    TextAndImage Block

--- src/components/ProjectBuilder.jsx
+++ src/components/ProjectBuilder.jsx
@@ -38,6 +38,7 @@ export const query = graphql`
 
       ...BlockImageFullWidth
       ...BlockProjectInfo
+      ...BlockTextAndImage
     }
   }
 `;
---
+++ src/components/blocks/TextAndImage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+import Image from "../Image";
+
+const TextAndImage = ({ data }) => {
+  const { title, text, image, hasBackground, imageAlign } = data;
+
+  return (
+    <div
+      className={`block-text-and-image${hasBackground && ` has-background`}`}
+    >
+      <header>
+        <h2 className="h4">{title}</h2>
+        <p className="text-normal">{text?.content}</p>
+      </header>
+
+      <figure className={`align-${imageAlign.toLowerCase()}`}>
+        <Image src={image} alt={image?.title} />
+      </figure>
+    </div>
+  );
+};
+
+export default TextAndImage;
+
+export const query = graphql`
+  fragment BlockTextAndImage on ContentfulBlockTextAndImage {
+    title
+    text {
+      content: text
+    }
+    image {
+      title
+      gatsbyImageData(
+        width: 1440
+        placeholder: BLURRED
+        formats: [AUTO, WEBP, AVIF]
+      )
+    }
+    hasBackground
+    imageAlign
+  }
+`;
