0c67b159ef776bd2b360c3e482ed365d77cb15ee
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-03-24 13:38:33 +1300

    PageTitle Block styles

--- src/components/blocks/PageTitle.jsx
+++ src/components/blocks/PageTitle.jsx
@@ -11,6 +11,21 @@ const options = {
     [BLOCKS.HEADING_1]: (node, children) => (
       <h1 className="h1">{addColour(children)}</h1>
     ),
+    [BLOCKS.HEADING_2]: (node, children) => (
+      <h1 className="h2">{addColour(children)}</h1>
+    ),
+    [BLOCKS.HEADING_3]: (node, children) => (
+      <h1 className="h3">{addColour(children)}</h1>
+    ),
+    [BLOCKS.HEADING_4]: (node, children) => (
+      <h1 className="h4">{addColour(children)}</h1>
+    ),
+    [BLOCKS.HEADING_5]: (node, children) => (
+      <h1 className="h5">{addColour(children)}</h1>
+    ),
+    [BLOCKS.HEADING_6]: (node, children) => (
+      <h1 className="h6">{addColour(children)}</h1>
+    ),
   },
 };
 
@@ -18,7 +33,7 @@ const PageTitle = ({ data }) => {
   const { content } = data;
 
   return (
-    <div>
+    <div className="block-page-title">
       <RichText content={content} options={options} />
     </div>
   );
