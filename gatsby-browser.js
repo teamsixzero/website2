a236e0532ca6be15344d3eeffdb4e9f762fad8b1
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-02-26 10:45:02 +1100

    Add page view tracking in gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -24,4 +24,20 @@ export const onRouteUpdate = () => {
   if (mainEl?.scrollTop) {
     mainEl.scrollTop = 0;
   }
+
+  if (process.env.NODE_ENV !== "production") {
+    return null;
+  }
+
+  const pagePath = location
+    ? location.pathname + location.search + location.hash
+    : undefined;
+
+  setTimeout(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("event", "page_view", { page_path: pagePath });
+    }
+  }, 100);
+
+  return true;
 };
